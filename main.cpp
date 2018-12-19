a807467b069200059e49a23da4517627c72d084c
Maxwell <maxperryg@gmail.com>
2018-12-19 14:28:52 -0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+//
+//  main.cpp
+//  Inverted_Index_CPP
+//
+//  Created by Maxwell Perry Grossman on 12/19/18.
+//  Copyright © 2018 Maxwell Perry Grossman. All rights reserved.
+//
+
+#include "iostream"
